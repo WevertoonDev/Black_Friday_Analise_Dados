4d54a5beb1523a7d08bd005afd95112cd33c98eb
WevertoonDev <wevertonm22@icloud.com>
2026-07-11 21:54:33 -0300

    categorias

---
+++ Consultas_Sql/Consulta_Basica_categorias.sql
@@ -0,0 +1,10 @@
+SELECT * from categorias;
+
+SELECT * from fornecedores;
+
+SELECT * FROM marcas;
+
+SELECT COUNT(*) from produtos;
+SELECT COUNT(*) AS Vendas_Totais from vendas;
+SELECT COUNT(*) AS Qtd_Categorias from categorias;
+
