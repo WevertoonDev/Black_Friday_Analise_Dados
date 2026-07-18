2babc253a99d4913cd77897124189e1a10501b78
WevertoonDev <wevertonm22@icloud.com>
2026-07-17 23:10:18 -0300

    Ciclos_vendas

---
+++ Demanda_Black_Friday/Ciclos_Vendas.sql
@@ -0,0 +1,14 @@
+SELECT strftime('%Y/%m',data_venda) from vendas LIMIT 5;
+
+SELECT DISTINCT(strftime('%Y', data_venda)) AS Ano from vendas
+ORDER by Ano;
+
+SELECT strftime('%Y', data_venda) as Ano, COUNT(id_venda) as Total_Vendas 
+from vendas
+GROUP by Ano
+order by Ano;
+
+SELECT strftime('%Y', data_venda) as Ano, strftime('%m',data_venda) as Mes, COUNT(id_venda) as Total_Vendas 
+from vendas
+GROUP by Ano, Mes
+order by Ano;
