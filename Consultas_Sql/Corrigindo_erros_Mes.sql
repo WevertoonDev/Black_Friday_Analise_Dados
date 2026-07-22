9556c2184647e72415918137c4616081bfe0e9b7
WevertoonDev <wevertonm22@icloud.com>
2026-07-21 22:33:51 -0300

    Corrigindo_Erro_Mes

---
+++ Consultas_Sql/Corrigindo_erros_Mes.sql
@@ -0,0 +1,16 @@
+SELECT * from categorias;
+SELECT * from fornecedores;
+SELECT * FROM marcas;
+SELECT * from produtos;
+SELECT * from vendas;
+
+SELECT COUNT(*) from produtos;
+SELECT COUNT(*) AS Vendas_Totais from vendas;
+SELECT COUNT(*) AS Qtd_Categorias from categorias;
+SELECT COUNT(*) AS Qtd_Clientes from clientes;
+SELECT COUNT(*) AS Qtd_Fornecedores from fornecedores;
+
+SELECT * from vendas LIMIT 5;
+SELECT * from fornecedores LIMIT 5;
+SELECT * from produtos LIMIT 5;
+
