2d6cd5aa0ec126b0179d9e902bd6930f3f328c71
WevertoonDev <wevertonm22@icloud.com>
2026-08-06 22:25:03 -0300

    Mes_Desc

---
+++ Consultas_Sql/Mes_Desc.sql
@@ -0,0 +1,30 @@
+SELECT strftime('%Y', v.data_venda) AS 'Ano', c.nome_categoria AS Nome_Categoria, COUNT(iv.produto_id) AS Qtd_Vendas
+FROM itens_venda iv
+JOIN vendas v ON v.id_venda = iv.venda_id
+JOIN produtos p ON p.id_produto = iv.produto_id
+JOIN categorias c ON c.id_categoria = p.categoria_id
+WHERE strftime('%m', v.data_venda) = '11'
+GROUP BY Nome_Categoria, "Ano"
+ORDER BY 'Ano', Qtd_Vendas ASC
+;
+
+SELECT strftime('%Y', v.data_venda) AS 'Ano', c.nome_categoria AS Nome_Categoria, COUNT(iv.produto_id) AS Qtd_Vendas
+FROM itens_venda iv
+JOIN vendas v ON v.id_venda = iv.venda_id
+JOIN produtos p ON p.id_produto = iv.produto_id
+JOIN categorias c ON c.id_categoria = p.categoria_id
+WHERE strftime('%m', v.data_venda) = '11'
+GROUP BY Nome_Categoria, "Ano"
+ORDER BY 'Ano', Qtd_Vendas DESC
+;
+
+
+SELECT strftime('%Y/%m', v.data_venda) AS "Ano/Mes",COUNT(iv.produto_id) AS Qtd_Vendas
+  FROM itens_venda iv
+  JOIN vendas v ON v.id_venda = iv.venda_id
+  JOIN produtos p ON p.id_produto = iv.produto_id
+  JOIN fornecedores f ON f.id_fornecedor = p.fornecedor_id
+  WHERE f.nome = 'NebulaNetworks'
+  GROUP BY f.nome, "Ano/Mes"
+  ORDER BY "Ano/Mes", Qtd_Vendas
+;
