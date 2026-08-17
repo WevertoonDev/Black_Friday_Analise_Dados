d8a76b5aaf051a641a88d99ac2307a08de7c0660
WevertoonDev <wevertonm22@icloud.com>
2026-08-16 23:54:27 -0300

    teste

--- Demanda_Black_Friday/Teste/Teste.sql
+++ Demanda_Black_Friday/Teste/Teste.sql
@@ -1,26 +1,31 @@
-SELECT * from categorias;
-SELECT * from produtos;
-SELECT * from marcas;
-SELECT * from fornecedores;
-SELECT * from itens_venda;
-SELECT * FROM clientes;
+SELECT strftime('%Y/%m',data_venda) from vendas LIMIT 5;
 
-SELECT COUNT(*) from produtos;
-SELECT COUNT(*) as Vendas_Totais from vendas;
+SELECT DISTINCT(strftime('%Y', data_venda)) AS Ano from vendas
+ORDER by Ano;
 
-SELECT COUNT(*) as Qtd, 'Categorias' as Tabela FROM categorias
-UNION ALL
-SELECT COUNT(*) as Qtd, 'Clientes' as Tabela FROM clientes
-UNION ALL
-SELECT COUNT(*) as Qtd, 'Fornecedores' as Tabela FROM fornecedores
-UNION ALL
-SELECT COUNT(*) as Qtd, 'ItensVenda' as Tabela FROM itens_venda
-UNION ALL
-SELECT COUNT(*) as Qtd, 'Marcas' as Tabela FROM marcas
-UNION ALL
-SELECT COUNT(*) as Qtd, 'Produtos' as Tabela FROM produtos
-UNION ALL
-SELECT COUNT(*) as Qtd, 'Vendas' as Tabela FROM vendas;
+SELECT strftime('%Y', data_venda) as Ano, COUNT(id_venda) as Total_Vendas 
+from vendas
+GROUP by Ano
+order by Ano DESC
 
-SELECT * from vendas LIMIT 5;
+SELECT strftime('%Y%m', data_venda) as Ano, COUNT(id_venda) as Total_Vendas 
+from vendas
+GROUP by Ano
+order by Ano;
 
+SELECT strftime('%Y', data_venda) as Ano, strftime('%m',data_venda) as Mes, COUNT(id_venda) as Total_Vendas 
+from vendas
+GROUP by Ano, Mes
+order by Ano;
+
+SELECT strftime('%Y', data_venda) as Ano, strftime('%m',data_venda) as Mes, COUNT(id_venda) as Total_Vendas 
+from vendas
+WHERE Mes = '01' or Mes = '11' or Mes = '12'
+GROUP by Ano, Mes
+order by Ano DESC 
+
+SELECT strftime('%Y', data_venda) as Ano, strftime('%m',data_venda) as Mes, COUNT(id_venda) as Total_Vendas 
+from vendas
+WHERE Mes = '01' or Mes = '06' or Mes = '12'
+GROUP by Ano, Mes
+order by Ano;
